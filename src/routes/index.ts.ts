7e09be9848914501e5126cd191fb3f6c1788338a
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-02-12 23:56:24 -0500

    feat(routes): added client, providers and workers routes

--- src/routes/index.ts.ts
+++ src/routes/index.ts.ts
@@ -3,4 +3,7 @@ import pucServices from "@app/controllers/PQPUCController";
 
 export default function addUserRoutes(app : Application) {
     app.use('/api/v1/puc', pucServices)
+    app.use('/api/v1/client')
+    app.use('/api/v1/providers')
+    app.use('/api/v1/workers')
 }
