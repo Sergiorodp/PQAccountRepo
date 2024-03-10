1afd49a0b3b10feedce0a77419085b2cd49d9915
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-03-09 21:04:24 -0500

    feat(process.env): added jwt key string

--- src/config/envVars.ts
+++ src/config/envVars.ts
@@ -2,6 +2,7 @@ interface IAppEnvs extends NodeJS.ProcessEnv {
     MONGO_URI?: string
     HASH_KEY?: string
     PORT?: string
+    JWT_KEY?: string // 256 rand bits
 }
 
 const envs: IAppEnvs = process.env
