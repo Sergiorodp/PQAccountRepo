44441ad31e542aa220f5cc94060ce443bf300975
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-03-11 21:30:09 -0500

    feat(AuthController, package.json): create logIn Controller, added morgan for log

--- src/controllers/PQAuthController.ts
+++ src/controllers/PQAuthController.ts
@@ -1,9 +1,24 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import authBusiness from "@app/business/PQAuthBusiness";
+import { HTTPCODES } from "@app/utils/httpCodes";
 
 const router = Router()
 
 console.log(`[[ AUTH LOADED ]]`)
+
+function logIn( req: Request, res: Response){
+     //#region AUDITORIA DE ENTRADA
+    // TODO 
+    //#endregion
+    if(req.body){
+        
+    } else {
+        res.status(HTTPCODES.badRequest).send('No body found')
+    }
+    //#region AUDITORIA DE SALIDA
+    // TODO
+    //#endregion
+}
 
 router.get(`/health`, authBusiness.healthCheck)
 router.get(`/`)
