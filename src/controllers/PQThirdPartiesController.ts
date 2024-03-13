b19be98572b33544d6573fed19905f2b2c4ae5aa
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-03-12 22:04:33 -0500

    feat(ThirdPartyPerson*): lint corrections, refactor variables (No complete)

--- src/controllers/PQThirdPartiesController.ts
+++ src/controllers/PQThirdPartiesController.ts
@@ -1,33 +1,32 @@
-import { Router, Request, Response} from "express";
-import { HTTPCODES } from "@app/utils/httpCodes"
+import { Router, type Request, type Response } from 'express'
+import { HTTPCODES } from '@app/utils/httpCodes'
 import { createThirdPartyPersonBusinessV1 } from '@app/business/PQThirdPartyPersonBusiness'
 
 const thirdPartiesRouter = Router()
 
-console.log(`[[ THIRD PARTIES LOADED ]]`)
+console.log('[[ THIRD PARTIES LOADED ]]')
 
-function createThirdPartiePersonControllerv1( req: Request, res: Response){
-    //#region AUDITORIA DE ENTRADA
-    // TODO 
-    //#endregion
-    if(req.body){
-        createThirdPartyPersonBusinessV1(req)
-        .then( message => {
-            if(typeof thirdPartiesRouter !== 'string'){
-                res.status(HTTPCODES.created).send(message)
-            }else{
-                res.status(HTTPCODES.badRequest).send(message)
-            }
-        })
-    } else {
-        res.status(HTTPCODES.badRequest).send('No body found')
-    }
-    //#region AUDITORIA DE SALIDA
-    // TODO
-    //#endregion
+function createThirdPartiePersonControllerv1 (req: Request, res: Response) {
+  // #region AUDITORIA DE ENTRADA
+  // TODO
+  // #endregion
+  if (req.body) {
+    createThirdPartyPersonBusinessV1(req)
+      .then(message => {
+        if (typeof thirdPartiesRouter !== 'string') {
+          res.status(HTTPCODES.created).send(message)
+        } else {
+          res.status(HTTPCODES.badRequest).send(message)
+        }
+      })
+  } else {
+    res.status(HTTPCODES.badRequest).send('No body found')
+  }
+  // #region AUDITORIA DE SALIDA
+  // TODO
+  // #endregion
 }
 
 thirdPartiesRouter.post('/create/v1', createThirdPartiePersonControllerv1)
 
 export default thirdPartiesRouter
-
--- src/dataBase/mongoDB/CRUD/PQ_CRUD_THIRD_PARTY.ts
+++ src/dataBase/mongoDB/CRUD/PQ_CRUD_THIRD_PARTY.ts
@@ -1,25 +1,25 @@
-import { TPQThirdPartyPerson } from "@app/models/PQThirdPartyPersonModel";
-import MDBThirdPartyPersonConnection, { IThirdPartyPersonSchema } from "../Schemas/PQThirdPartyPersonSchemas";
-import { IPQThirdPartiesRepository } from "@app/dataBase/repoInterfaces/PQRepositoryInterfaces";
+import { type TPQThirdPartyPerson } from '@app/models/PQThirdPartyPersonModel'
+import MDBThirdPartyPersonConnection, { type IThirdPartyPersonSchema } from '../Schemas/PQThirdPartyPersonSchemas'
+import { type IPQThirdPartiesRepository } from '@app/dataBase/repoInterfaces/PQRepositoryInterfaces'
 
 export class MongoThirdPersonRepository implements IPQThirdPartiesRepository {
+  private static instance: MongoThirdPersonRepository
 
-    private static instance: MongoThirdPersonRepository;
+  private constructor () {
+    // Private constructor to prevent direct instantiation
+  }
 
-    private constructor() {
-        // Private constructor to prevent direct instantiation
-      }
-
-      public static getInstance(): MongoThirdPersonRepository {
-        if (!MongoThirdPersonRepository.instance) {
-            MongoThirdPersonRepository.instance = new MongoThirdPersonRepository();
-        }
-        return MongoThirdPersonRepository.instance;
-      }
-
-    async create( TPPerson : TPQThirdPartyPerson ): Promise<IThirdPartyPersonSchema>{
-        const mongoRes = await MDBThirdPartyPersonConnection.create(TPPerson)
-        const formatObj = {...mongoRes.toJSON(), _id : '', __v : ''}
-        return formatObj
+  public static getInstance (): MongoThirdPersonRepository {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!MongoThirdPersonRepository.instance) {
+      MongoThirdPersonRepository.instance = new MongoThirdPersonRepository()
     }
+    return MongoThirdPersonRepository.instance
+  }
+
+  async create (TPPerson: TPQThirdPartyPerson): Promise<IThirdPartyPersonSchema> {
+    const mongoRes = await MDBThirdPartyPersonConnection.create(TPPerson)
+    const formatObj = { ...mongoRes.toJSON(), _id: '', __v: '' }
+    return formatObj
+  }
 }
