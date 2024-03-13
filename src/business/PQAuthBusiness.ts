22bc164d93adac02d7889a2653e84f92330365e4
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-03-12 22:02:47 -0500

    feat(Auth*): develop business logic (No complete)

--- src/business/PQAuthBusiness.ts
+++ src/business/PQAuthBusiness.ts
@@ -1,55 +1,130 @@
-import { Request, Response } from "express"
-import { IResponseBusiness } from "@app/models/PQResponseBusinessModel"
-import { TPQLogIn, LogInSchema } from "@app/models/PQAuthModel"
-import argon from'argon2'
-import jwt from "jsonwebtoken"
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { type Request, type Response } from 'express'
+import { type IResponseBusiness } from '@app/models/PQResponseBusinessModel'
+import { type TPQLogIn, LogInSchema } from '@app/models/PQAuthModel'
+import { getPQUserByEmailRepoV1 } from '@app/dataBase/PQUserRepository'
+import envs from '@app/config/envVars'
+import argon from 'argon2'
+import jwt, { type SignOptions } from 'jsonwebtoken'
+import { type PQUserRepoResponse } from '@app/models/PQUserModel'
 
-//#region GET PUC BY CODE
-async function userLoginBusinessV1(req: Request){
+// #region GET PUC BY CODE
+async function userLoginBusinessV1 (req: Request): Promise<IResponseBusiness<object>> {
+  let canContinue = true
+  let errorHandler: Error | null = null
+  let response: IResponseBusiness<object> = {
+    message: '',
+    success: false,
+    detail: [{}]
+  }
+  let userInfo: TPQLogIn | null = null
+  let getUserDBResponse: PQUserRepoResponse | null = null
+  let checkPassword: boolean = false
 
-    let canContinue = true;
-    let response : IResponseBusiness<object> = {
-        message: '',
-        success: false,
-        detail: [{}]
+  // #region IN AUDIT
+  // #endregion
+
+  // #region DATA CHECK
+  if (canContinue) {
+    const userParse = LogInSchema.safeParse(req.body)
+    if (userParse.success) {
+      userInfo = userParse.data
+    } else {
+      canContinue = false
     }
-    let userInfo : TPQLogIn | null = null
-    //#region IN AUDIT
-    //#endregion
+  }
+  // #endregion
 
-    //#region DATA CHECK
-    if(canContinue) {
-        const userParse = LogInSchema.safeParse(req.body)
-        if(userParse.success){
-            userInfo = userParse.data;
-        }else{
-            canContinue = false
-        }
+  // #region QUERY USER
+  if (canContinue) {
+    try {
+      getUserDBResponse = await getPQUserByEmailRepoV1(userInfo?.email ?? '')
+    } catch (e) {
+      canContinue = false
+      errorHandler = e as Error
     }
-    //#endregion
+  }
+  // #endregion
 
-    //#region QUERY USER
-    if(canContinue){
-        
+  // #region VERIFY PASSWORD
+  if (canContinue) {
+    try {
+      if (getUserDBResponse?.password && userInfo?.password) {
+        checkPassword = await argon.verify(getUserDBResponse?.password, userInfo?.password)
+      } else {
+        canContinue = false
+        // TODO handle error
+      }
+    } catch (e) {
+      canContinue = false
+      // TODO handle error
     }
-    //#endregion
-    //#region VERIFY PASSWORD
-    if(canContinue){
-        argon.verify('', userInfo?.password as string)
-    } 
-    //#endregion
+  }
+  // #endregion
+
+  // #region GENERATE JWT
+  if (canContinue && checkPassword) {
+    try {
+      const currentDate = new Date()
+      const creationDate = Math.floor(currentDate.getTime() / 1000)
+      const expirationDate = Math.floor(currentDate.setHours(currentDate.getHours() + 10) / 1000)
+      const options: SignOptions = {
+        algorithm: 'HS256',
+        expiresIn: '10h'
+      }
+      const payload = {
+        userName: getUserDBResponse?.userName,
+        name: getUserDBResponse?.name,
+        email: getUserDBResponse?.email,
+        createToken: creationDate,
+        expireToken: expirationDate
+      }
+      if (envs.JWT_KEY != null) {
+        jwt.sign(payload, envs.JWT_KEY, options)
+      } else {
+        canContinue = false
+        // TODO handle no JWT_KEY error
+      }
+    } catch {
+      canContinue = false
+      // TODO handle error cant sign token
+    }
+  }
+  // #endregion
+
+  // #region RESPONSE
+  if (canContinue) {
+    response.success = canContinue
+    if (getUserDBResponse) {
+      response = {
+        ...response,
+        detail: [getUserDBResponse]
+      }
+    } else {
+      response = {
+        ...response,
+        message: 'No user created, DB problem'
+      }
+    }
+  } else {
+    response.success = canContinue
+    response.message = errorHandler?.toString() ?? 'Unknown Error'
+  }
+
+  return response
+  // #endregion
 }
-//#endregion
+// #endregion
 
-//#region healthCheck
-function healthCheck(_ : Request, res: Response){
-    res.send({
-        responde: 'PUC API IS WORKING'
-    })
+// #region healthCheck
+function healthCheck (_: Request, res: Response): void {
+  res.send({
+    responde: 'PUC API IS WORKING'
+  })
 }
-//#endregion
+// #endregion
 
 export default {
-    healthCheck,
-    userLoginBusinessV1
+  healthCheck,
+  userLoginBusinessV1
 }
--- src/controllers/PQAuthController.ts
+++ src/controllers/PQAuthController.ts
@@ -1,26 +1,32 @@
-import { Request, Response, Router } from "express";
-import authBusiness from "@app/business/PQAuthBusiness";
-import { HTTPCODES } from "@app/utils/httpCodes";
+import { type Request, type Response, Router } from 'express'
+import authBusiness from '@app/business/PQAuthBusiness'
+import { HTTPCODES } from '@app/utils/httpCodes'
 
 const router = Router()
 
-console.log(`[[ AUTH LOADED ]]`)
+console.log('[[ AUTH LOADED ]]')
 
-function userLoginControllerV1( req: Request, res: Response){
-     //#region AUDITORIA DE ENTRADA
-    // TODO 
-    //#endregion
-    if(req.body){
-        
-    } else {
-        res.status(HTTPCODES.badRequest).send('No body found')
-    }
-    //#region AUDITORIA DE SALIDA
-    // TODO
-    //#endregion
+function userLoginControllerV1 (req: Request, res: Response): void {
+  // #region AUDITORIA DE ENTRADA
+  // TODO
+  // #endregion
+  if (req.body) {
+    authBusiness.userLoginBusinessV1(req)
+      .then(businessRes => {
+        res.status(203).send(businessRes)
+      })
+      .catch(e => {
+        res.status(500)
+      })
+  } else {
+    res.status(HTTPCODES.badRequest).send('No body found')
+  }
+  // #region AUDITORIA DE SALIDA
+  // TODO
+  // #endregion
 }
 
-router.get(`/health`, authBusiness.healthCheck)
-router.get(`/`)
+router.get('/health', authBusiness.healthCheck)
+router.post('/logIn/v1', userLoginControllerV1)
 
 export default router
