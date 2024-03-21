a510887be9dab3cf78f7966cfa32217e34955a3d
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-03-20 22:37:42 -0500

    feat(ThirdParty): added new handle error type

--- src/business/PQThirdPartyPersonBusiness.ts
+++ src/business/PQThirdPartyPersonBusiness.ts
@@ -1,12 +1,11 @@
 import { PQThirdPartyPersonSchema } from '@app/models/PQThirdPartyPersonModel'
 import { createPQThirdPartyPersonRepo } from '@app/dataBase/PQThirdPartyPersonRepository'
 import { type Request } from 'express'
-import { type ZodError } from 'zod'
 import { type IResponseBusiness } from '@app/models/PQResponseBusinessModel'
 
 export async function createThirdPartyPersonBusinessV1 (req: Request): Promise<IResponseBusiness> {
   let canContinue: boolean = true
-  let errorHandler: ZodError | null = null
+  let errorHandler: Error | null = null
   let response: IResponseBusiness = {
     message: '',
     success: false,
@@ -32,6 +31,7 @@ export async function createThirdPartyPersonBusinessV1 (req: Request): Promise<I
       }
     } catch {
       canContinue = false
+      // TODO error handler
     }
   }
   // #endregion
@@ -59,8 +59,27 @@ export async function createThirdPartyPersonBusinessV1 (req: Request): Promise<I
   // #endregion
 }
 
-export async function getThirdPartiePerson (id: string) {
-  // #region AUDITORIA DE ENTRADA
-  // TODO
+export async function getThirdPartiePerson (id: string): Promise<IResponseBusiness> {
+  const canContinue: boolean = true
+  const errorHandler: Error | null = null
+  let response: IResponseBusiness = {
+    message: '',
+    success: false,
+    detail: [{}]
+  }
+
+  // #region RESPONSE
+  if (canContinue) {
+    response.success = canContinue
+    response = {
+      ...response,
+      detail: [{/* */}]
+    }
+  } else {
+    response.success = canContinue
+    response.message = errorHandler ?? 'Unknown Error'
+  }
+
+  return response
   // #endregion
 }
