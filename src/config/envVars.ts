9ac9a30995e10d20df0ffe560d033ea924110fe3
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-03-29 21:25:23 -0500

    feat(envs): refactor JWT_SECRET name

--- src/config/envVars.ts
+++ src/config/envVars.ts
@@ -1,8 +1,8 @@
 interface IAppEnvs extends NodeJS.ProcessEnv {
-    MONGO_URI?: string
-    HASH_KEY?: string
-    PORT?: string
-    JWT_KEY?: string // 256 rand bits
+  MONGO_URI?: string
+  HASH_KEY?: string
+  PORT?: string
+  JWT_SECRET?: string // 256 rand bits
 }
 
 const envs: IAppEnvs = process.env
