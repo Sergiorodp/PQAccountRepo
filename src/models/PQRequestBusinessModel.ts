74f134eebde35d97a506a5b03210ca8aa46f3e57
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-03-31 22:37:27 -0500

    feat(Request): added new request model

---
+++ src/models/PQRequestBusinessModel.ts
@@ -0,0 +1,12 @@
+import { type Request } from 'express'
+
+export interface IPQRequestBusiness extends Request {
+  PQUserInfo?: IPQUserInfo
+}
+
+interface IPQUserInfo {
+  userName: string
+  name: string
+  email: string
+  role: string
+}
