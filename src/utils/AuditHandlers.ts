b864c71105d7abf34287f41041917dd74fab6ef9
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-05-06 23:27:58 -0500

    feat(Audit_Util): added requestType to know if the request is in or outbound request

--- src/utils/AuditHandlers.ts
+++ src/utils/AuditHandlers.ts
@@ -1,6 +1,6 @@
 import AuditModel, { type TAuditRecordRequestModel } from '@app/models/PQAuditModel'
 
-function saveRequest (baseRequest: TAuditRecordRequestModel): void {
+function saveRequest (baseRequest: TAuditRecordRequestModel, requestType: string): void {
   // #region CHECK DATA
   const AuditSafeParse = AuditModel.AuditSchema.safeParse(baseRequest)
   // #endregion
